e8a75807d2e937365b4f839ac73f2236b852b615
lubinets <lubineckarina@gmail.com>
2023-02-12 00:57:35 +0100

    Search updated

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Search.css";
 
 export default function Search() {
-  let [city, setCity] = useState("London");
-
-  function changeCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
   return (
-    <div className="Search">
+    <form className="Search">
       <input
         type="search"
         className="search-bar"
         placeholder="Type your city..."
-        onChange={changeCity}
       />
+
       <input type="submit" className="submit-button" value="Search" />
-    </div>
+    </form>
   );
 }
